b22930a85cf397cd996974fc6ec55a6c94875317
MoritzGruber <m.gruber@cyclus.digital>
2023-07-24 16:14:25 +0200

    Initial commit

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { RichTextRenderer } from '@caisy/rich-text-svelte-renderer';
+	import DocumentLink from "./DocumentLink"
+	export let data: { title: string; text: any };
+	console.log(data, JSON.stringify(data));
+</script>
+
+<svelte:head>
+	<title>{data.title}</title>
+</svelte:head>
+
+<section>
+	<h1>{data.title}</h1>
+	{#if data.text && data.text.json}
+		<RichTextRenderer node={data.text.json} 
+				overwrites={{ documentLink: DocumentLink }}
+		connections={data?.text?.connections}
+		/>
+	{/if}
+</section>
+
+<style>
+</style>
